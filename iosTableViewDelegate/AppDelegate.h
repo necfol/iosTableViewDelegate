07cb21129742774a1b6f9c88f533746e812a0717
necfol <necfoll@gmail.com>
2017-08-17 16:48:10 +0800

    initial

---
+++ iosTableViewDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosTableViewDelegate
+//
+//  Created by necfol on 2017/8/17.
+//  Copyright © 2017年 Necfol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosTableViewDelegate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosTableViewDelegate
+//
+//  Created by necfol on 2017/8/17.
+//  Copyright © 2017年 Necfol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
